4a5f4ba2b156f76e54fce4340ca9f1466414f326
AllenHirmiz <allenaliraqi@gmail.com>
2023-08-06 00:56:53 +1000

    Clone Note Maker Repo into a single Folder

--- server.js
+++ server.js
@@ -1,44 +1,49 @@
-const PORT = process.env.PORT || 3002;
-const fs = require('fs');
-const path = require('path');
-
 const express = require('express');
 const app = express();
+const PORT = process.env.port || 3001;
+const path = require('path');
+const fs = require('fs');
+const SavedNotes = require('./db/db.json');
 
-const allNotes = require('./db/db.json');
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('/api/notes', (req, res) => {
-    res.json(allNotes.slice(1));
-});
+app.get('/api/notes', (req, res) => 
+    res.json(
+        SavedNotes.slice(1)
+    )
+);
+app.get('/', (req, res) => 
+    res.sendFile(
+        path.join(__dirname, './public/index.html')
+    )
+);
+app.get('/notes', (req, res) => 
+    res.sendFile(
+        path.join(__dirname, './public/notes.html')
+    )
+);
+app.get('*', (req, res) => 
+    res.sendFile(
+        path.join(__dirname, './public/index.html')
+    )
+);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-function createNewNote(body, notesArray) {
+function saveNewNotes(body, notesArray) {
     const newNote = body;
-    if (!Array.isArray(notesArray))
+    if (!Array.isArray(notesArray)) {
         notesArray = [];
-    
-    if (notesArray.length === 0)
+    }
+    if (notesArray.length === 0) {
         notesArray.push(0);
-
+    }
+        
     body.id = notesArray[0];
     notesArray[0]++;
 
     notesArray.push(newNote);
+    
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
         JSON.stringify(notesArray, null, 2)
@@ -47,7 +52,7 @@ function createNewNote(body, notesArray) {
 }
 
 app.post('/api/notes', (req, res) => {
-    const newNote = createNewNote(req.body, allNotes);
+    const newNote = saveNewNotes(req.body, SavedNotes);
     res.json(newNote);
 });
 
@@ -68,10 +73,10 @@ function deleteNote(id, notesArray) {
 }
 
 app.delete('/api/notes/:id', (req, res) => {
-    deleteNote(req.params.id, allNotes);
+    deleteNote(req.params.id, SavedNotes);
     res.json(true);
 });
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+    console.log(`Server running on port ${PORT}`);
 });
