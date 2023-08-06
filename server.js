214833b87fbf457ef38f803f66bdc21a64fb4e33
AllenHirmiz <allenaliraqi@gmail.com>
2023-08-06 23:19:07 +1000

    trying to deploy my app day 2

--- server.js
+++ server.js
@@ -1,32 +1,82 @@
+const PORT = process.env.port || 3001;
 const express = require('express');
-const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
-
-const PORT = process.env.PORT || 3001;
-
 const app = express();
+const path = require('path');
+const fs = require('fs');
+const SavedNotes = require('./db/db.json');
 
-// Import custom middleware, "cLog"
-app.use(clog);
-
-// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
-
 app.use(express.static('public'));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get('/api/notes', (req, res) => 
+    res.json(
+        SavedNotes.slice(1)
+    )
+);
+app.get('/', (req, res) => 
+    res.sendFile(
+        path.join(__dirname, './public/index.html')
+    )
+);
+app.get('/notes', (req, res) => 
+    res.sendFile(
+        path.join(__dirname, './public/notes.html')
+    )
+);
+app.get('*', (req, res) => 
+    res.sendFile(
+        path.join(__dirname, './public/index.html')
+    )
 );
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-);
+function saveNewNotes(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray)) {
+        notesArray = [];
+    }
+    if (notesArray.length === 0) {
+        notesArray.push(0);
+    }
+        
+    body.id = notesArray[0];
+    notesArray[0]++;
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
-);
+    notesArray.push(newNote);
+    
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+
+app.post('/api/notes', (req, res) => {
+    const newNote = saveNewNotes(req.body, SavedNotes);
+    res.json(newNote);
+});
+
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+
+            break;
+        }
+    }
+}
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, SavedNotes);
+    res.json(true);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
